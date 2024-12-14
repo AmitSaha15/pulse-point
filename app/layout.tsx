20548d2facca84b1097f33bd2d79ef3e4ef541d2
Amit Saha <asaha.bwn2000@gmail.com>
2024-12-14 19:44:26 +0530

    init setup + error fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from '@/lib/utils';
+import { ThemeProvider } from "@/components/theme-provider"
 
 const fontSans = Plus_Jakarta_Sans({
   subsets: ["latin"],
@@ -22,11 +23,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head />
       <body
         className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <h1 className="text-white text-4xl underline">Home Page.</h1>
+      <Button>Click me!</Button>
     </div>
   );
 }
